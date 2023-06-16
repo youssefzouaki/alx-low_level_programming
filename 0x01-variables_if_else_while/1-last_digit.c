1d7314313b7fd317c884edab65d0a05083e22e1a
Youssef ZOUAKI <youssefzouaki@gmail.com>
2023-06-16 16:13:14 -0700

    correct an error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Print the last digit of randum number
+ *main - Print the last digit of randum number
  *
  *Return: Always 0
  */
@@ -19,7 +19,7 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, last);
 		else if (last > 5)
 			printf("Last digit of %d is %d and is greater than 5\n", n, last);
-	        	else if (last < 6)
+			else if (last < 6)
 				printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	return (0);
 }
