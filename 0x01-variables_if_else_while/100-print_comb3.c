5709db725b87a737c505fa0add5260a911e40500
Youssef ZOUAKI <youssefzouaki@gmail.com>
2023-06-17 06:21:42 -0700

    c program

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,25 @@
--e #include <stdio.h>
+#include <stdio.h>
+/**
+ * main - prints all  possible different combinations of two digits,
+ * followed by a new line.
+ *
+ * Return: Return 0
+ */
+int main(void)
+{
+	int l;
+	int i;
+
+	for (l = 48; l < 58 ; l++)
+	{
+		for (i = l+1; i < 58; i++)
+		{
+			putchar(l);
+			putchar(i);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
