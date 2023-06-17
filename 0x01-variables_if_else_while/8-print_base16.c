251044a8d026b23e0180c7fbe871218942526760
Youssef ZOUAKI <youssefzouaki@gmail.com>
2023-06-16 17:03:44 -0700

    c pro

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,23 @@
--e #include <stdio.h>
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 16 starting from 0,
+ * followed by a new line.
+ *
+ * Return: Return 0
+ */
+int main(void)
+{
+	int l;
+	char c;
+
+	for (l = 48; l < 58 ; l++)
+	{
+		putchar(l);
+	}
+	for (c = 'a'; c < 'f' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
