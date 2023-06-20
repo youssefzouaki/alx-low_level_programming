afb216f4a9780a4408b0dc2be02ee12167eb70e7
root <root@DESKTOP-4RBK34I.localdomain>
2023-06-20 15:40:31 +0100

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,14 +7,14 @@
 
 void print_alphabet(void)
 {
-    char c = 'a';
+	char c = 'a';
 
-    while (c <= 'z')
-    {
-        _putchar(c);
-        c++;
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
 
-    }
-    _putchar('\n');
+	}
+	_putchar('\n');
 
 }
