7027a82f38dc764f107dabe96c2485bba785c7e6
root <root@DESKTOP-4RBK34I.localdomain>
2023-06-21 03:09:12 +0100

    all fonction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
  * times_table -  prints the 9 times table, starting with 0.
- * 
- * 
 */
 
 void times_table(void)
 {
 	int i, j, k, u, d;
+
 	for (i = 0; i < 10 ; i++)
 	{
 		for (j = 0; j < 10; j++)
