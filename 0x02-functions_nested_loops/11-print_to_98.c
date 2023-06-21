2bf6d8358ad0d9e08eeedf718dba763c188721c4
root <root@DESKTOP-4RBK34I.localdomain>
2023-06-21 13:04:18 +0100

    correvt error fonction 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 -  prints all natural numbers from n to 98, 
  * followed by a new line.
@@ -8,50 +9,22 @@
 
 void print_to_98(int n)
 {
-	int i, u, d;
-
-	for (i = n; i < 99; i++)
+	int i;
+	if (n < 98)
 	{
-		if (i < 10 && i > -10)
-			{
-				if (i < 0)
-				{
-					_putchar('-');
-					_putchar(-i + '0');
-				}
-				else
-				{
-					_putchar(i + '0');
-				}
-				if (i != 98)
-				{
-				_putchar(',');
-				_putchar(' ');
-				}
-			}
-		else
+		for (i = n; i < 98; i++)
 		{
-			if (i >= 0)
-				{
-				u = i % 10;
-				d = (i - u) / 10;
-				_putchar(d + '0');
-				_putchar(u + '0');
-				}
-			else
-				{
-				u = -i % 10;
-				d = (-i - u) / 10; 
-				_putchar('-');
-				_putchar(d + '0');
-				_putchar(u + '0');
-				}
-				if (i != 98)
-				{
-				_putchar(',');
-				_putchar(' ');
-				}
+			printf("%d, ", i);
 		}
+		printf("98");
 	}
-	_putchar('\n');
+	if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}
+		printf("98");
+	}
+	printf("\n");
 }
