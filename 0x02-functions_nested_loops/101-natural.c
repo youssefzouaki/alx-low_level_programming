dba960ba562865c4f1678869da7c0ae00b927e78
root <root@DESKTOP-4RBK34I.localdomain>
2023-06-21 21:54:40 +0100

    correct fonction 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 /**
- * main - 
+ * main - computes and prints the sum of all the multiples of
+ * 3 or 5 below 1024 (excluded),
+ * followed by a new line.
  * Return: 0
 */
 
 int main(void)
 {
-    int m = 3, sum = 0;
+	int m = 3, sum = 0;
 
-    while (m < 1024)
-    {
-        if (m % 3 == 0 || m % 5 == 0)
-       {
-        sum+=m;
-       }
-       m++;
-    }
-    printf("%d\n", sum);
+	while (m < 1024)
+	{
+		if (m % 3 == 0 || m % 5 == 0)
+		{
+		sum+=m;
+		}
+	m++;
+	}
+	printf("%d\n", sum);
 
-    return (0);
+	return (0);
 }
