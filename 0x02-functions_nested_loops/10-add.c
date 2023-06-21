036c66c1f4dac9c11fad87b03d7eb7034743294e
root <root@DESKTOP-4RBK34I.localdomain>
2023-06-21 03:10:38 +0100

    all fonction

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,11 @@
 /**
  * add -  adds two integers and returns the result.
- *  @a: int to be added to b
+ * @a: int to be added to b
  * @b: int to be added to a
+ * Return: sum
 */
 
 int add(int a, int b)
 {
-	return (a+b);
+	return (a + b);
 }
