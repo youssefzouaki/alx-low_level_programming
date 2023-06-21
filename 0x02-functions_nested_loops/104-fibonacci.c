5341c7717753e8fb5dac23c4cc1082cf7bc27fa0
root <root@DESKTOP-4RBK34I.localdomain>
2023-06-22 00:16:38 +0100

    fonction 104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main -  prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2,
+ * followed by a new line.
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	unsigned long fibo, fibo1, fibo2;
+
+	fibo = 1;
+	fibo1 = 1;
+	for (i = 1; i < 98 ; i++)
+	{
+		printf("%lu, ", fibo);
+		fibo2 = fibo1;
+		fibo1 = fibo;
+		fibo = fibo1 + fibo2;
+	}
+	printf("%lu", fibo);
+	printf("\n");
+
+	return (0);
+}
