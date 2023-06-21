e66a5b7cc5938e208c024a92b2195e758c10ee34
root <root@DESKTOP-4RBK34I.localdomain>
2023-06-21 13:07:48 +0100

    correct error fonction 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98 - prints all natural numbers from n to 98, 
+ * print_to_98 - prints all natural numbers from n to 98,
  * followed by a new line.
  *  @n: print from this number
  *
@@ -10,7 +10,7 @@
 void print_to_98(int n)
 {
 	int i;
-	
+
 	if (n <= 98)
 	{
 		for (i = n; i < 98; i++)
