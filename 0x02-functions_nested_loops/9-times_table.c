05af5df65b372ed843871fe61756098767b01f15
root <root@DESKTOP-4RBK34I.localdomain>
2023-06-21 03:20:37 +0100

    all fonction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,28 +5,31 @@
 
 void times_table(void)
 {
-	int i, j, k, u, d;
+	int i, j, k;
 
 	for (i = 0; i < 10 ; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
-			if (k < 10)
+			if (j == 0)
 			{
 				_putchar(k + '0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
 			}
-			else
+			if (k < 10 && j != 0)
 			{
-				u = k % 10;
-				d = (k - u) / 10;
-				_putchar(d + '0');
-				_putchar(u + '0');
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+				
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
