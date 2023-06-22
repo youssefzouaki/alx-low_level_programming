acc96169eb1c91f317e946acb4a44d9be31042e0
root <root@DESKTOP-4RBK34I.localdomain>
2023-06-22 23:39:56 +0100

    fonction 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_number - prints an integer.
+ * @n: input n
+*/
+
+void print_number(int n)
+{
+	int i, p;
+
+	i = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	p = n;
+	while (p / 10 != 0)
+	{
+		p /= 10;
+		i *= 10;
+	}
+	while (i != 1)
+	{
+		_putchar(n / i + '0');
+		n %= i;
+		i /= 10;
+	}
+	_putchar(n % 10 + '0');
+}
