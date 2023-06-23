2ba67d65ae1ea0619c2b18d951068ec6dc2f38f7
root <root@DESKTOP-4RBK34I.localdomain>
2023-06-23 01:34:50 +0100

    correct f7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,19 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n ; i++)
+	if (n > 0)
+	{
+		for (i = 0; i < n ; i++)
 	{
 		for (j = 0; j < i ; j++)
 		{
 			_putchar(' ');
 		}
 		_putchar('\\');
+		_putchar('\n');
 	}
+	}
+	else
 	_putchar('\n');
+	
 }
