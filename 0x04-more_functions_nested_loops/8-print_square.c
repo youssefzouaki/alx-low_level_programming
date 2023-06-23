f43c9c9083d03ebfd3f2eab8b31e16c980ad8136
root <root@DESKTOP-4RBK34I.localdomain>
2023-06-23 02:00:51 +0100

    correct f8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,7 +8,9 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size ; i++)
+	if (size > 0)
+	{
+		for (i = 0; i < size ; i++)
 	{
 		for (j = 0; j < size ; j++)
 		{
@@ -16,4 +18,8 @@ void print_square(int size)
 		}
 		_putchar('\n');
 	}
+	}
+	else
+	_putchar('\n');
+
 }
