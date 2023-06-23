a64ee629c21f1deb0c8089adb88ab49e7a88e4ed
root <root@DESKTOP-4RBK34I.localdomain>
2023-06-23 01:22:12 +0100

    correct

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,19 @@
 void more_numbers(void)
 {
 	int l, i;
+	
 	for (i = 0 ; i < 10; i++)
 	{
-		for (l = 0; l <= 14 ; l++)
+		for (l = 0; l < 10 ; l++)
 		{
 		_putchar(l + '0');
 		}
+		for (l = 0; l < 5 ; l++)
+		{
+			_putchar('1');
+			_putchar(l + '0');
+		}
+
 		_putchar('\n');
 	}
 }
