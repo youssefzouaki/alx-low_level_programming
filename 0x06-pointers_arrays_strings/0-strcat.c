1fe67503a08155ff1e6073a1b6c7548bae5c4cde
root <root@DESKTOP-4RBK34I.localdomain>
2023-06-30 15:59:02 +0100

    f0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strcat - appends the src string to the dest string, overwriting the terminating null byte (\0) at the end of dest,
+ * and then adds a terminating null byte
+ * @dest: pointer to a character
+ * @src: pointer to a character
+ * Return: 
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+
+	while (*dest != '\0')
+	{
+		dest++;
+		i++;
+	}
+
+	while (*src != '\0')
+    {
+		*dest = *src;
+		src++;
+		dest++;
+		i++;
+
+	}
+
+	*dest = '\0';
+	dest = dest - i;
+	return (dest);
+
+}
