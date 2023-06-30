f7f2fc39af9581182ff6bfded2951e91cafba953
root <root@DESKTOP-4RBK34I.localdomain>
2023-06-30 18:52:55 +0100

    f2

--- 0x06-pointers_arrays_strings/1-main.c
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-    char s1[98] = "Hello ";
+    char s1[98] = "Hollo";
     char s2[] = "World!\n";
     char *ptr;
 
     printf("%s\n", s1);
     printf("%s", s2);
-    ptr = _strncat(s1, s2, 1);
+    ptr = _strncat(s1, s2, 7);
     printf("%s\n", s1);
     printf("%s", s2);
     printf("%s\n", ptr);
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,37 @@
-#include "main.h"
-
 /**
- * 
+ * _strncpy - is similar to the _strcat function,
+ * except that:
+ * it will use at most n bytes from src; and
+ * src does not need to be null-terminated if
+ * it contains n or more bytes
+ * @dest: pointer to a character
+ * @src: pointer to a character
+ * @n: an integre
  *
- * Return: 
+ * Return: pointer to a character
  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+
+	while (j < n && *src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+		j++;
+	}
+	while (j < n)
+	{
+		*dest = '\0';
+		dest++;
+		j++;
+	}
+
+	dest = dest - n;
+
+	return (dest);
+}
