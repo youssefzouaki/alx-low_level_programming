c2a0729f793212ef3a60d16264d13a1b4c4b2b21
root <root@DESKTOP-4RBK34I.localdomain>
2023-06-30 19:38:58 +0100

    f3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,26 @@
-#include "main.h"
-
 /**
- * 
+ * _strcmp - compares two strings.
+ * @s1: pointer to a string
+ * @s2: pointer to a string
  *
- * Return: 
+ * Return: an integre.
  */
+
+int _strcmp(char *s1, char *s2)
+{
+	int cmp;
+
+	while (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == '\0' && *s2 != '\0')
+	cmp = - *s2;
+	else if (*s1 != '\0' && *s2 == '\0')
+	cmp = *s1;
+	else
+	cmp = *s1 - *s2;
+
+	return (cmp);
+}
