fa1fa46c6a206d0cb5bc9c916aa0d305d9bd61f8
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-02 18:38:06 +0100

    correct error betty of f100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -16,10 +16,10 @@ char *rot13(char *s)
 		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == a[j])
-            {
-                s[i] = b[j];
-                break;
-            }
+			{
+				s[i] = b[j];
+				break;
+			}
 		}
 
 	}
