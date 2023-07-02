6bc2fc200c8b030787fed044b0827f4484215f48
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-02 18:22:24 +0100

    f100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,29 @@
-#include "main.h"
-
 /**
- * 
+ * rot13 - encodes a string into rot-13.
+ * @s: pointer to a string
  *
- * Return: 
+ * Return: pointer to a string
  */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char *a = "abcdefghijklmABCDEFGHIJKLMnopqrstuvwxyzNOPQRSTUVWXYZ";
+	char *b = "nopqrstuvwxyzNOPQRSTUVWXYZabcdefghijklmABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == a[j])
+            {
+                s[i] = b[j];
+                break;
+            }
+		}
+
+	}
+
+	return (s);
+
+}
