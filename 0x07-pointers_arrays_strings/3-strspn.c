2665555c459e0f016242da5c2f1c8f631b45dde9
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-03 16:59:30 +0100

    f3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: pointer to the memory area
+ * @accept: pointer to a string
+ *
+ * Return: number of bytes in the initial segment of s.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (*s != '\0')
+	{
+		while (*accept != '\0')
+		{
+			if (*s == *accept)
+			{
+				i++;
+				break;
+			}
+			accept++;
+			j++;
+		}
+	if (j == 0)
+	{
+		break;
+	}
+	accept -= j;
+	s++;
+	}
+	return (i);
+}
