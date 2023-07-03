1a51e3f4daaa8a89cc90bdc05fd056212774d42d
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-03 15:50:01 +0100

    f2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include <stddef.h>
+/**
+ * _strchr - locates a character in a string.
+ * @s: pointer to the string s
+ * @c: the character c
+ *
+ * Return: pointer to the first occurrence of
+ * the character c in the string s, or
+ * NULL if the character is not found.
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		break;
+		else
+		s++;
+	}
+	if (*s == '\0')
+	{
+		return NULL;
+	}
+	return (s);
+}
