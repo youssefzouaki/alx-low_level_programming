86ad6809da5cff6669a35b74f68a7a28aab2e889
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-04 15:41:45 +0100

    f5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,39 +5,31 @@
  *
  * Return: a pointer to the beginning of the located substring, or
  * NULL if the substring is not found.
-*/
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
-
-	i = 0;
-
-	if (*needle == '\0' || *haystack == '\0')
-	{
-		haystack = 0;
-	}
+	int i = 0;
 
 	while (*haystack != '\0')
 	{
-	while (needle[i] != '\0' && haystack[i] != '\0')
-	{
-		if (haystack[i] != needle[i])
+		while (needle[i] != '\0' && haystack[i] != '\0')
 		{
-			haystack += i + 1;
-			i = 0;
+			if (haystack[i] != needle[i])
+			{
+				haystack += i + 1;
+				i = 0;
+				break;
+			}
+			i++;
+		}
+		if (needle[i] == '\0')
+		{
 			break;
 		}
-		i++;
-	}
-	if (needle[i] == '\0')
-	{
-		break;
-	}
 	}
 	if (needle[i] != '\0')
 	{
 		haystack = 0;
 	}
-
 	return (haystack);
 }
