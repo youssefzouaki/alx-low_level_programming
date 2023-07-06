f9c1b2450d6cbc43db429f901449640ba171b672
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-06 01:31:56 +0100

    f4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+/**
+ * _strlen_recursion - returns the value of x^y.
+ * @x: integer
+ * @y: integer
+ *
+ * Return: value of x^y.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
