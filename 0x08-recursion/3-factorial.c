8342b112681732479982f99d69ffe1b3b951f6b2
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-06 01:22:04 +0100

    f3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+/**
+ * factorial - returns the factorial of a given number.
+ * @n: The number
+ *
+ * Return: returns the factorial of a given number.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	
+	if (n == 1)
+	{
+		return(1);
+	}
+	return(n * factorial(n - 1));
+}
