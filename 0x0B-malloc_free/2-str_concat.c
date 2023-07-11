296484894b3e0a6e91ef574507a77a4900f9bd66
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-11 13:42:32 +0100

    f2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include "main.h"
-
 /**
  * str_concat - concatenates two strings
  * @s1: string to concatenate
@@ -11,32 +10,32 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *c;
-    int i, j, len1, len2;
+	char *c;
+	int i, j, len1, len2;
 
-    len1 = 0;
-    len2 = 0;
+	len1 = 0;
+	len2 = 0;
 
-    while (s1 || s1[len1])
-    {
-        len1++;
-    }
-    while (s2 || s2[len2])
-    {
-        len2++;
-    }
-    c = malloc(sizeof(char) * (len1 + len2 + 1));
-    if (c == NULL)
-        return (NULL);
+	while (s1 || s1[len1])
+	{
+		len1++;
+	}
+	while (s2 || s2[len2])
+	{
+		len2++;
+	}
+	c = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (c == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-    {
-        c[i] = s1[i];
-    }
-    for (j = i; j < len2 + i; j++)
-    {
-        c[j] = s2[j];
-    }
-    c[len1 + len2 + 1] = '\0';
-    return (c);
+	for (i = 0; i < len1; i++)
+	{
+		c[i] = s1[i];
+	}
+	for (j = i; j < len2 + i; j++)
+	{
+		c[j] = s2[j];
+	}
+	c[len1 + len2 + 1] = '\0';
+	return (c);
 }
