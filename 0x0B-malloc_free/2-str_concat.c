cf5bbbacff95f5fd5d28178744b367c40a983318
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-11 13:49:30 +0100

    f2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,11 +16,11 @@ char *str_concat(char *s1, char *s2)
 	len1 = 0;
 	len2 = 0;
 
-	while (s1 || s1[len1])
+	while (s1 && s1[len1])
 	{
 		len1++;
 	}
-	while (s2 || s2[len2])
+	while (s2 && s2[len2])
 	{
 		len2++;
 	}
