b87dc7ac8e702547cd12f62f091dc555c6b10fc9
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-11 18:56:36 +0100

    f4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,13 +10,13 @@
  */
 void free_grid(int **grid, int height)
 {
-    int i;
+	int i;
 
-    if (grid == NULL || height == 0)
+	if (grid == NULL || height == 0)
 		return;
 
-    for (i = 0; i < height; i++)
-        free(grid[i]);
+	for (i = 0; i < height; i++)
+		free(grid[i]);
 
-    free(grid);
+	free(grid);
 }
