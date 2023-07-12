fb4cd5ef8a1018b8113be699dda8b6f7a7df8b96
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-12 03:39:39 +0100

    f101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,41 +1,47 @@
 #include <stdlib.h>
 /**
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ *
+ * Return: number of words
+ */
+int count_word(char *s)
+{
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+
+	return (w);
+}
+/**
  * strtow - splits a string into words.
  * @str: pointer to array of size ac
  *
  * Return:  a pointer to a new string, or NULL if it fails
 */
-
 char **strtow(char *str)
 {
 	char **s, *t;
-	int i, j, h, k, len, start, end;
+	int i, j, h = 0, k = 0, len = 0, start, end;
 
-	h = 0;
-	k = 0;
-
-	while (str[i])
-	{
-		j = 0;
-		while (str[i] != ' ' && str[i])
-		{
-			i++;
-			j++;
-		}
-		if (j)
-			h++;
-		
-		if (str[i] == '\0')
-		{
-			len = i;
-			break;
-		}
-		i++;
-	}
-	len = i;
+	while (*(str + len))
+		len++;
+	h = count_word (str);
 	if (h == 0)
 		return (NULL);
-	s = malloc(h * sizeof(char *) );
+	s = malloc((h + 1) * sizeof(char *) );
 	if (s == NULL)
 		return (NULL);
 	j = 0;
@@ -64,5 +70,4 @@ char **strtow(char *str)
 	}
 	s[k] = NULL;
 	return (s);
-
 }
