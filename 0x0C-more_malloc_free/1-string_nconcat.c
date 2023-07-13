08323edf926507e89fb7aa0a7c6624f123bf2a28
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-13 23:48:15 +0100

    f1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,27 +18,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	unsigned int i, len1 = 0, len2 = 0;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
 	while (s1[len1])
 		len1++;
-	
 	while (s2[len2])
 		len2++;
-
 	if (n > len2)
 		n = len2;
-
 	s = malloc(sizeof(char) * (len1 + n + 1));
 	if (s == NULL)
 		return (NULL);
-
 	for (i = 0; i < len1; i++)
-	{
 		s[i] = s1[i];
-	}
 	for (i = len1; i < len1 + n; i++)
-	{
 		s[i] = s2[i - len1];
-	}
 	s[len1 + n] = '\0';
 	return (s);
 }
