bf5a914b26c9cff149223d696d0cb0f7433f132e
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-14 02:57:59 +0100

    f100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,17 +1,12 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 /**
  * _realloc - reallocates a memory block using malloc and free
  * @ptr: a pointer to the memory previously allocated with a call to malloc: malloc(old_size)
  * @old_size: the size, in bytes, of the allocated space for ptr
  * @new_size: the new size, in bytes of the new memory block
  *
- * Return: If new_size > old_size, the “added” memory should not be initialized
- * If new_size == old_size do not do anything and return ptr
- * If ptr is NULL, then the call is equivalent to malloc(new_size), for all values of old_size and new_size
- * If new_size is equal to zero, and ptr is not NULL, then the call is equivalent to free(ptr). Return NULL
+ * Return: ptr if new_size == old_size, or Null if
+ * new_size is equal to zero, and ptr is not NULL.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
@@ -20,20 +15,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-
 	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
 	if (!ptr)
 		return (malloc(new_size));
-
 	new_p = malloc(new_size);
 	if (!new_p)
 		return (NULL);
-
 	old_p = ptr;
 	if (new_size < old_size)
 	{
