61ff3e9f1b94884f6dacbac6b84f482b3625cf03
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-18 03:53:31 +0100

    new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,12 +13,28 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	if (name == NULL || owner == NULL)
-		return (NULL);
-	
+	int len1 = 0, len2 = 0;
+
+	while (name[len1])
+	len1++;
+	while (owner[len2])
+	len2++;
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
+	d->name = malloc(sizeof(char) * (len1 + 1));
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	d->owner = malloc(sizeof(char) * (len2 + 1));
+	if (d->owner == NULL)
+	{
+		free(d);
+		free(d->name);
+		return (NULL);
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
