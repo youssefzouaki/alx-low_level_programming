e129483133bf25ef2dd75c4c96758c48106edfac
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-18 03:34:13 +0100

    new_dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog -  initialize a variable of type struct dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Return: nothing.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
