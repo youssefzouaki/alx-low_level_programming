abbdda7ef22e15ea01333829e5ef39d98cdd3271
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-20 02:26:12 +0100

    all

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: pointer to the printing function
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if(!name || !f)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+
+/**
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	unsigned int i;
+	int a, b, result;
+	int (*op)(int, int);
+
+	if(argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op = get_op_func(argv[2]);
+	if(!op)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if((op == op_div || op == op_mod) && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = op(a, b);
+	printf("%d\n", result);
+
+	return (0);
+
+}
