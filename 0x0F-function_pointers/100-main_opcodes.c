2c6e2e4fb260387e6e86e09dcdb4e0afbb296da8
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-20 03:10:51 +0100

    adv

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,24 +10,24 @@
  */
 int main(int argc, char *argv[])
 {
-    int i, n;
-    char *arr;
+	int i, n;
+	char *arr;
 
-    if(argc != 2)
+	if(argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-    n = atoi(argv[1]);
-    if(n < 0)
+	n = atoi(argv[1]);
+	if(n < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-    arr = (char*)main;
-    for (i = 0; i < n - 1; i++)
-        printf("%02hhx ", arr[i]);
-    printf("%02hhx\n", arr[n - 1]);
-    
-    return (0);
+	arr = (char*)main;
+	for (i = 0; i < n - 1; i++)
+		printf("%02hhx ", arr[i]);
+	printf("%02hhx\n", arr[n - 1]);
+
+	return (0);
 }
