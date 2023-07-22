c5a55ad8216040440727f350b7fc85feed3e46e2
root <root@DESKTOP-4RBK34I.localdomain>
2023-07-22 01:13:44 +0100

    all

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			else if (separator && i == 0)
 		 		printf("%d", va_arg(args, int));
 			else
-				printf("%d%s", separator, va_arg(args, int));
+				printf("%s%d", separator, va_arg(args, int));
 		}
 	printf("\n");
 	va_end(args);
