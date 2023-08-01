2f45885754c4692bb9d025770962351a82d546e6
youssefzouaki <youssefzouaki@gmail.com>
2023-08-01 15:02:54 +0100

    task 1 betty

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,12 +8,12 @@
  */
 size_t listint_len(const listint_t *h)
 {
-    size_t n = 0;
+	size_t n = 0;
 
-    while (h)
-    {
-        h = h->next;
-        n++;
-    }
-    return (n);
+	while (h)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
 } 
