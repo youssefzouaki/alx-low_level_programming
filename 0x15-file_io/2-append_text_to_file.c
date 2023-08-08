222335e3c73247d47989e03575f35b64a389cace
youssefzouaki <youssefzouaki@gmail.com>
2023-08-08 19:25:06 +0100

    task 2 fix error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,7 +9,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int a, len == 0;
+	int a, len = 0;
 	ssize_t leen;
 
 	if (filename == NULL)
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		while (text_content[len])
 			len++;
 
-		leen = write(a, text_content, len)
+		leen = write(a, text_content, len);
 	}
 
 	close(a);
