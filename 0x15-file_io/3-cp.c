cef869ca24ee69f638d9b1dc2fd07489e569c0ca
youssefzouaki <youssefzouaki@gmail.com>
2023-08-10 17:15:58 +0100

    task 3 correct err3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,7 +4,7 @@
 
 #define USAGE "Usage: cp file_from file_to\n"
 #define ERR_NOREAD "Error: Can't read from file %s\n"
-#define ERR_NOWRITE "Error: Can't write to file %s\n"
+#define ERR_NOWRITE "Error: Can't write to %s\n"
 #define ERR_NOCLOSE "Error: Can't close fd %d\n"
 
 char *create_buffer(char *file);
